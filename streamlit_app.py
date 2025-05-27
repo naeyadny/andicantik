50fccfcc79c9eb7e799df0699eaebb7d9d9bf440
naeyadny <andiahmadafkar@gmail.com>
2025-05-27 11:50:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 st.title("belajar make up")
 st.write(
-    "ayo belajar make up bersama andi"
-    )
-    st.image("IMG-20250518-WA0010.jpeg", width=600)
+"ayo belajar bersama andi")
+st.image("IMG-20250518-WA0010.jpeg", width=600)
