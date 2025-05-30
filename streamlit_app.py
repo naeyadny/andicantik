963ec4a94b283e8987e090a42eab2d3cebc291bf
naeyadny <andiahmadafkar@gmail.com>
2025-05-30 18:11:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,5 @@ import streamlit as st
 st.title("belajar make up")
 st.write("ayo belajar bersama andi")
 st.image("IMG-20250518-WA0010.jpeg", width=600)
-
 st.title("Berikut beberapa tips dasar untuk melakukan make up")
 st.write("1. Persiapan: Pastikan wajah bersih dan lembab sebelum memulai makeup. Gunakan primer untuk membuat makeup lebih tahan lama. 2. Foundation: Pilih foundation yang sesuai dengan warna kulit Anda. Aplikasikan secara merata untuk menyamarkan noda dan ketidaksempurnaan. 3. Concealer: Gunakan concealer untuk menutupi noda dan lingkaran hitam di bawah mata. 4. Eyeshadow: Pilih warna eyeshadow yang sesuai dengan warna mata Anda. Aplikasikan secara bertahap untuk menciptakan efek yang diinginkan. 5. Mascara: Gunakan mascara untuk mempertebal dan memperpanjang bulu mata. 6. Lipstik atau Lip Gloss: Pilih warna lipstik atau lip gloss yang sesuai dengan warna kulit dan preferensi Anda.")
