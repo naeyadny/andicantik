7dce41e7bbae36d075c30301f7f921a08fe089fd
naeyadny <andiahmadafkar@gmail.com>
2025-05-30 18:15:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,11 @@ st.write("1. Persiapan: Pastikan wajah bersih dan lembab sebelum memulai makeup.
 3. Concealer: Gunakan concealer untuk menutupi noda dan lingkaran hitam di bawah mata. 
 4. Eyeshadow: Pilih warna eyeshadow yang sesuai dengan warna mata Anda. Aplikasikan secara bertahap untuk menciptakan efek yang diinginkan.
 5. Mascara: Gunakan mascara untuk mempertebal dan memperpanjang bulu mata. 6. Lipstik atau Lip Gloss: Pilih warna lipstik atau lip gloss yang sesuai dengan warna kulit dan preferensi Anda.")
+st.title("Rating makeup gambar tersebut") 
+st.header("Aplikasi menilai ganjil/genap") 
+angka= st.number_input("70:", value=0, step=1) 
+
+if(angka % 2) == 0:
+  st.write(f"{2,4,6,8,10} adalah Rating makeup gambar tersebut") 
+else:
+  st.write(f"{1,3,5,7,9} adalah jumlah angka ganjil")
